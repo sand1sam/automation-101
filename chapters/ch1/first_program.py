c246964ac7dd091f1fcef19b0fff7a944af705e4
Sandra <samuels.sandra@gmail.com>
2025-07-02 22:45:32 +0100

    Challenge 1

---
+++ chapters/ch1/first_program.py
@@ -0,0 +1,8 @@
+# ask user for first name
+first_name = input("What is your first name? ")
+
+# ask user for last name
+last_name = input("What is your last name? ")
+
+#print their full name
+print(f"Your full name is  {first_name} {last_name}")
